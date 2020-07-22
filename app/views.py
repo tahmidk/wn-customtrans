b788a91abc06491872339771073a3d01569ae174
Tahmid Khan <tahmidkhan3141@gmail.com>
2020-07-22 01:45:10 -0700

    Implements general layout of libraries page
    - Implements general layout of Libraries sub page sans and backend
    - Cleans up units used to rely primarily on rem instead of vh and vw
    - Implements inclusion of "Back" button determined by Flask

--- app/views.py
+++ app/views.py
@@ -10,15 +10,68 @@ from flask import render_template
 from flask import url_for
 from app import app
 
+# Internal imports
+# from scripts import wn_customtrans as ctrans
+# from scripts import configdata as config
+
+
 # This is the route for the main page
 @app.route("/")
 def index():
-	return render_template('index.html', title='Index Page')
+	return render_template('index.html',
+		title='CustomTrans')
 
 # The following 4 routes are the routes for the 4 main page buttons
 @app.route("/library")
 def library():
-	return "TODO: Implement Library Page"
+	series=[
+		{
+            "abbr": "Dummy",
+            "title": "This is a Sample Novel Title",
+            "current": 1,
+            "latest": 100,
+            "code": "n7057gi"
+        },
+        {
+            "abbr": "BocchiIdol",
+            "title": "I'm the Best Idol in Japan",
+            "current": 0,
+            "latest": 45,
+            "code": "n9946fx"
+        },
+        {
+            "abbr": "FlagCrush",
+            "title": "The Adventurer Contracted to the Strongest Deity Crushes Flags",
+            "current": 145,
+            "latest": 1206,
+            "code": "n3404fh"
+        },
+        {
+            "abbr": "CHJK",
+            "title": "When My Childhood Friend Becomes JK",
+            "current": 2,
+            "latest": 28,
+            "code": "n4893gi"
+        },
+        {
+            "abbr": "WhiteRabbit",
+            "title": "The Strongest Incompetent Reaches for the Top",
+            "current": 1,
+            "latest": 10,
+            "code": "n0737ga"
+        },
+        {
+            "abbr": "Surrounded",
+            "title": "Surrounded By Four",
+            "current": 50,
+            "latest": 57,
+            "code": "n9450ge"
+        },
+	]
+	return render_template('library.html',
+		title="Library",
+		back_href=url_for('index'),
+		series=series)
 
 @app.route("/dictionaries")
 def dictionaries():
